440d40fecca7ad21fa141bd91f2ac9a6be3abf01
Christian Pehle <christian.pehle@gmail.com>
2016-01-09 01:41:12 +0100

    Difference camera image

--- main.cpp
+++ main.cpp
@@ -2,20 +2,25 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+
 int main(int argc, char** argv) {
   cv::VideoCapture camera(0);
+  cv::Mat lastframe;
+  cv::Mat difframe;
   cv::Mat frame;
   if (!camera.isOpened()) {
     return -1;
   }
   cv::namedWindow("Source");
-  
+  cv::namedWindow("Difference");
+
+  camera >> frame;
   for (;;) {
+    frame.copyTo(lastframe);
     camera >> frame;
+    cv::absdiff(lastframe, frame, difframe);
     cv::imshow("Source", frame);
-    if (cv::waitKey(1) == 27) {
-      break;
-    } 
+    cv::imshow("Difference", difframe);
   }
   return 0;
 }
